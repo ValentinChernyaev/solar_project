47fd1e9d5708d15e91a3c3948c4ea18653486c22
ValentinChernyaev <sergeich1997@yandex.ru>
2016-11-15 11:39:14 +0300

    Commit

--- solar_main.py
+++ solar_main.py
@@ -10,6 +10,9 @@ from solar_input import *
 perform_execution = False
 """Флаг цикличности выполнения расчёта"""
 
+
+
+"""Заголовок по умолчанию"""
 physical_time = 0
 """Физическое время от начала расчёта.
 Тип: float"""
@@ -38,7 +41,7 @@ def execution():
     for body in space_objects:
         update_object_position(space, body)
     physical_time += time_step.get()
-    displayed_time.set("%.1f" % physical_time + " seconds gone")
+    displayed_time.set("%.1f" % physical_time + " прошло секунд")
 
     if perform_execution:
         space.after(101 - int(time_speed.get()), execution)
@@ -50,11 +53,11 @@ def start_execution():
     """
     global perform_execution
     perform_execution = True
-    start_button['text'] = "Pause"
+    start_button['text'] = "Пауза"
     start_button['command'] = stop_execution
 
     execution()
-    print('Started execution...')
+    print('Программа запущена..')
 
 
 def stop_execution():
@@ -63,9 +66,9 @@ def stop_execution():
     """
     global perform_execution
     perform_execution = False
-    start_button['text'] = "Start"
+    start_button['text'] = "Пуск"
     start_button['command'] = start_execution
-    print('Paused execution.')
+    print('Выполнение приостановлено')
 
 
 def open_file_dialog():
@@ -73,6 +76,7 @@ def open_file_dialog():
     функцию считывания параметров системы небесных тел из данного файла.
     Считанные объекты сохраняются в глобальный список space_objects
     """
+    global title_hider
     global space_objects
     global perform_execution
     perform_execution = False
@@ -81,7 +85,12 @@ def open_file_dialog():
     in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
     space_objects = read_space_objects_data_from_file(in_filename)
     max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
+    # вычмсление масштаба отображения для первода в экранные координаты
     calculate_scale_factor(max_distance)
+     #  Подпись на холсте
+    update_system_name(space, title_header.title)
+
+
 
     for obj in space_objects:
         if obj.type == 'star':
@@ -97,56 +106,64 @@ def save_file_dialog():
     функцию считывания параметров системы небесных тел из данного файла.
     Считанные объекты сохраняются в глобальный список space_objects
     """
-    out_filename = asksaveasfilename(filetypes=(("Text file", ".txt"),))
+    out_filename = asksaveasfilename(title = "Введите название файла отчёта",
+    initialfile='Report.txt',filetypes=(("Text file", ".txt"),))
     write_space_objects_data_to_file(out_filename, space_objects)
 
 
-def main():
+
     """Главная функция главного модуля.
     Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
     """
-    global physical_time
-    global displayed_time
-    global time_step
-    global time_speed
-    global space
-    global start_button
+global physical_time
+global displayed_time
+global time_step
+global time_speed
+global space
+global start_button
 
-    print('Modelling started!')
-    physical_time = 0
+print('Моделирование запущено..')
+physical_time = 0
 
-    root = tkinter.Tk()
+root = tkinter.Tk()
     # космическое пространство отображается на холсте типа Canvas
-    space = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
-    space.pack(side=tkinter.TOP)
+    # ЛУЧШЕ ЗАДАТЬ ЯВНО РАЗМЕР ПОЛОТНА ИНАЧЕ НЕ ОТОБРАЖАЕТСЯ НИЖНИЙ ФРЕЙМ
+space = tkinter.Canvas(root, width=800, height=600, bg="black")
+space.pack(side=tkinter.TOP)
     # нижняя панель с кнопками
-    frame = tkinter.Frame(root)
-    frame.pack(side=tkinter.BOTTOM)
+frame = tkinter.Frame(root)
+frame.pack(side=tkinter.BOTTOM)
 
-    start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
-    start_button.pack(side=tkinter.LEFT)
+start_button = tkinter.Button(frame, text="Пуск", command=start_execution, width=6)
+start_button.pack(side=tkinter.LEFT)
+lab = Label(frame, text="Множитель времени")
+lab.pack(side=tkinter.LEFT)
+# Поле ввода шага времени чтобы работало нужно минимум 100 000
+time_step = tkinter.DoubleVar()
+time_step.set(100000)
+time_step_entry = tkinter.Entry(frame, width=8, bd=3, textvariable=time_step)
+time_step_entry.pack(side=tkinter.LEFT)
 
-    time_step = tkinter.DoubleVar()
-    time_step.set(1)
-    time_step_entry = tkinter.Entry(frame, textvariable=time_step)
-    time_step_entry.pack(side=tkinter.LEFT)
+time_speed = tkinter.DoubleVar()
+scale = tkinter.Scale(frame, variable=time_speed, length=100,
+from_=0,to=100,resolution=1,
+orient=HORIZONTAL)
 
-    time_speed = tkinter.DoubleVar()
-    scale = tkinter.Scale(frame, variable=time_speed, orient=tkinter.HORIZONTAL)
-    scale.pack(side=tkinter.LEFT)
 
-    load_file_button = tkinter.Button(frame, text="Open file...", command=open_file_dialog)
-    load_file_button.pack(side=tkinter.LEFT)
-    save_file_button = tkinter.Button(frame, text="Save to file...", command=save_file_dialog)
-    save_file_button.pack(side=tkinter.LEFT)
+scale.pack(side=tkinter.LEFT, fill="y")
 
-    displayed_time = tkinter.StringVar()
-    displayed_time.set(str(physical_time) + " seconds gone")
-    time_label = tkinter.Label(frame, textvariable=displayed_time, width=30)
-    time_label.pack(side=tkinter.RIGHT)
 
-    root.mainloop()
-    print('Modelling finished!')
+load_file_button = tkinter.Button(frame, text="Открыть файл..", command=open_file_dialog)
+load_file_button.pack(side=tkinter.LEFT)
+save_file_button = tkinter.Button(frame, text="Сохранить в файл...", command=save_file_dialog)
+save_file_button.pack(side=tkinter.LEFT)
 
-if __name__ == "__main__":
-    main()
+displayed_time = tkinter.StringVar()
+displayed_time.set(str(physical_time) + " секунд с момента запуска")
+time_label = tkinter.Label(frame, textvariable=displayed_time, width=30)
+time_label.pack(side=tkinter.RIGHT)
+
+root.mainloop()
+print('Моделирование завершено...')
+
+
